47bff93d386c86949a62aff2003e1e9956fc942d
Anatoly Buchatsky <buchatskij.anatoly@gmail.com>
2022-03-08 20:08:07 +0900

    Initialize yandex mapkit

--- app/src/main/java/com/example/maptest/MainActivity.kt
+++ app/src/main/java/com/example/maptest/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.maptest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.yandex.mapkit.MapKitFactory
+import com.yandex.mapkit.mapview.MapView
 
 class MainActivity : AppCompatActivity() {
 
+    private var mapview: MapView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        MapKitFactory.setApiKey(getString(R.string.yandex_map_key))
+        MapKitFactory.initialize(this)
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapview?.onStop()
+        MapKitFactory.getInstance().onStop()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapview?.onStart()
+        MapKitFactory.getInstance().onStart()
     }
 }
